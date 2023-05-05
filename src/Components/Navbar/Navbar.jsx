ab8f7eef9e5258055321b2c0b0dde416d2ff599f
Sheila-a <sheila.codes.m@gmail.com>
2023-05-05 15:56:29 +0100

    Fix merge

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import Logo from "../../assets/logo.png";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import design from "./style.module.css";
 import Search from "../Search/Search";
 
 const Navbar = () => {
   return (
     <div className={design.Navbar}>
-      <img src={Logo} alt="Logo" />
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
       <Search />
       <div className={design.NavEnd}>
-        <button>Sign Up</button>
-        <button>Sign In</button>
+        <Link to="/register">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="login">
+          {" "}
+          <button>Sign In</button>
+        </Link>
       </div>
     </div>
   );
